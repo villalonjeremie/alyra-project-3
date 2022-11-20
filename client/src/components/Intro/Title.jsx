d5a3c3d21694c14c74b43f5b89f9202bdf17e4aa
JeremieVillalon <villalon.jeremie@gmail.com>
2022-11-20 21:40:39 +0100

    init7

--- client/src/components/Intro/Title.jsx
+++ client/src/components/Intro/Title.jsx
@@ -1,7 +1,12 @@
+//import useEth from "../../contexts/EthContext/useEth";
+
 function Title() {
+  //const { state: { accounts } } = useEth();
+
   return (
     <div className="welcome">
       <h1> Voting Dapp </h1>
+      <span></span>
     </div>
   );
 }
